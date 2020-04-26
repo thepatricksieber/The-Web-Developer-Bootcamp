f9291f060036b7318edc8fae7d3146f00068f5e0
Patrick Sieber <31919032+thepatricksieber@users.noreply.github.com>
2020-04-26 21:08:39 +0200

    Selector Exercise JavaScript

---
+++ js/180_SelectorExercises.js
@@ -0,0 +1,12 @@
+var p1 = document.getElementById("first");
+
+var p2 = document.getElementsByClassName("special");
+
+var p3 = document.querySelector("p");
+
+var p4 = document.querySelectorAll("p");
+
+console.log(p1)
+console.log(p2[0])
+console.log(p3)
+console.log(p4[0])
